bdd1c4a675d7ea5e9b53795ca10df710a66ac12c
gustavovs88 <gustavovs88@gmail.com>
2024-04-23 19:21:37 -0300

    adiciona rota

---
+++ tests/integration/api/v1/status/get.test.js
@@ -0,0 +1,9 @@
+import { describe, it } from "node:test";
+import { strict as assert } from "node:assert";
+
+describe("GET to /api/v1/status", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/status");
+  it("should return 200", () => {
+    assert.equal(response.status, 200);
+  });
+});
