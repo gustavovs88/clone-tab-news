93e6e2cf91ba2d90c81b9f9e7ab56d5922f0fe1f
gustavovs88 <gustavovs88@gmail.com>
2024-05-20 21:01:24 -0300

    feat:  finished

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,33 @@
 import database from "#infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1+1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "valor" });
+  const updatedAt = new Date().toISOString();
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  const databaseMaxConnectionsResult = await database.query(
+    "SELECT setting::int FROM pg_settings WHERE name='max_connections';",
+  );
+  const databaseMaxConnectionsValue =
+    databaseMaxConnectionsResult.rows[0].setting;
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const databaseOpenConnectionsValue =
+    databaseOpenConnectionsResult.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: databaseMaxConnectionsValue,
+        opened_connections: databaseOpenConnectionsValue,
+      },
+    },
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -4,7 +4,13 @@ import status from "#api/v1/status/index.js";
 
 describe("GET to /api/v1/status", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
+  const body = await response.json();
   it("should return 200", () => {
     assert.equal(response.status, 200);
+    const parsedUpdatedAt = new Date(body.updated_at).toISOString();
+    assert.equal(body.updated_at, parsedUpdatedAt);
+    assert.equal(body.dependencies.database.version, "16.2");
+    assert.equal(body.dependencies.database.max_connections, 100);
+    assert.equal(body.dependencies.database.opened_connections, 1);
   });
 });
