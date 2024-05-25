d6db398af23e05f2271a7f044089a1539946f346
gustavovs88 <gustavovs88@gmail.com>
2024-05-25 10:29:52 -0300

    feat: use ssl on production database

--- infra/database.js
+++ infra/database.js
@@ -7,6 +7,7 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
     database: process.env.POSTGRES_DB,
+    ssl: process.env.NODE_ENV === "development" ? false : true,
   });
   await client.connect();
   try {
