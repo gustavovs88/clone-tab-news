06da33715b280e6164786a2d5d7f5c3f8e16ca89
gustavovs88 <gustavovs88@gmail.com>
2024-06-02 14:40:03 -0300

    feat: add more robust database connection settings

--- infra/database.js
+++ infra/database.js
@@ -7,7 +7,7 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
     database: process.env.POSTGRES_DB,
-    ssl: process.env.NODE_ENV === "development" ? false : true,
+    ssl: getSSLenv(),
   });
   await client.connect();
   try {
@@ -21,3 +21,12 @@ async function query(queryObject) {
 }
 
 export default { query: query };
+
+function getSSLenv() {
+  if (process.env.POSTGRES_CA) {
+    return {
+      ca: process.env.POSTGRES_CA,
+    };
+  }
+  return process.env.NODE_ENV === "development" ? false : true;
+}
